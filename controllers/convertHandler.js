564de05e8d01796b26cefb5ba6efb4fa103ca1e7
Amanda Pollard <AmandaPhere@gmail.com>
2025-01-20 15:35:32 -0500

    feat: main site still working, halfway through first unit tests. Had to get some example tests and am rewriting them to make sense for my code

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,124 +1,88 @@
+/*
+*
+*
+*       Complete the handler logic below
+*       
+*       
+*/
 
-
-
-
-
-
-
-
-
-const { init } = require("../server");
+let testReg = /[a-z]+|[^a-z]+/gi
 
 function ConvertHandler() {
-
-  //regex that splits numbers (as whole numbers, decimals, or fractions) from units
-  let testReg = /[a-z]+|[^a-z]+/gi
-  let testLetters = /[^a-z]+/gi //null = only letters
-  let initNum;
-  let initUnit;
-  let returnNum;
-  let returnUnit;
-
-
+  
   this.getNum = function(input) {
-    //get unit from arr, insert it as initNum value
-    let numRes = input.match(testReg)[0];
-    let result;
-    result = numRes;
-
-//if there's no unit, reject
-if (input.match(/[0-9]$/)) 
-  { return "invalid unit" } 
-
-//if it doesn't start with a number
-  if (numRes.match(/^[0-9]/) === null)
-  {return "invalid number"}
-let numReg = /\d/
-  //if there's no number, start with 1 as a base unit
-      if (numReg.test(result) === false)
-        { result = 1; return result}
-
-//check for special characters
-let stringify = input.toString();
-    //if doesn't end with a number
-
-
-//deal with division   
-if(result.toString().includes('/')){
-  let values = result.toString().split('/')
-  let perCheck = result.toString().split('.')
-
-//if there's more than one "/"  
-  if(values.length != 2)
-    { return 'invalid number'}
-
-//if there's more than 2 "." for every "/"
-  if ((perCheck.length - values.length) > 1)
-    { return "invalid number"}
-
-//for rejecting values with too mamy "."
-    if (numRes.includes("."))
-    {
-      //if value[0] has more than one "."
-      if (values[0].split(".").length > 2)
-      {return "invalid number"}
-      //if value[1] has more than one "."
-      if (values[1].split(".").length > 2)
-        {return "invalid number"}
-
+    var result;
+    
+    result = input.match(testReg)[0];
+    
+    let numRegex = /\d/
+    
+    if(numRegex.test(result) === false){
+      result = 1
+    }
+    
+    if(result.toString().includes('/')){
+      let values = result.toString().split('/')
+      if(values.length != 2){
+        return 'invalid number'
+      }
+      values[0] = parseFloat(values[0])
+      values[1] = parseFloat(values[1])
+      result = parseFloat((values[0]/values[1]).toFixed(5)) 
     }
 
-  values[0] = parseFloat(values[0])
-  values[1] = parseFloat(values[1])
-  result = parseFloat((values[0]/values[1]).toFixed(5)) 
-}
-
-    return result
-};
-
-
+    if(isNaN(result)){
+      return 'invalid number'
+    }
+    
+    return result;
+  };
+  
   this.getUnit = function(input) {
     let result;
+    
+    result = input.match(testReg)[1]
 
-    result = input.match(testReg)[1];
-  //if no unit was initially put in, reject
-    if (!result) 
-      { result = input.match(testReg)[0] }
+      //if no unit was initially put in, reject
+      if (!result) 
+        { result = input.match(testReg)[0] }
+    
+      //lowercase input to check validity    
+      let smallUnit = result.toLowerCase();
+      if (smallUnit === "gal" || smallUnit === "km" || smallUnit === "mi" || smallUnit === "lbs" || smallUnit === "kg")
+        { return result.toLowerCase(); }
 
-  //lowercase input to check validity    
-    let smallUnit = result.toLowerCase();
-
-    if (smallUnit === "gal" || smallUnit === "l" || smallUnit === "km" || smallUnit === "mi" || smallUnit === "lbs" || smallUnit === "kg")
-      { return result }
-
- 
-    result = 'invalid unit'
-       return result
+    if (smallUnit === "l")
+     {result = "L"; return result }
+    
+    result = "invalid unit"
+    return result;
   };
-
- 
+  
   this.getReturnUnit = function(initUnit) {
     let result;
+    getSmall = initUnit.toLowerCase();
 
-   if (initUnit === "mi")
+   if (getSmall === "mi")
         {result = "km"}
 
-   if (initUnit === "km")
+   if (getSmall === "km")
         {result = "mi"} 
 
-   if (initUnit === "gal")
-        {result = "l"}
+   if (getSmall === "gal")
+        {result = "L"}
     
-   if (initUnit === "l")
+   if (getSmall === "l")
     {result = "gal"}
     
-   if (initUnit === "lbs")
+   if (getSmall === "lbs")
     {result = "kg"}
     
-   if (initUnit === "kg")
+   if (getSmall === "kg")
     {result = "lbs"}
     return result
   };
+
 
   this.spellOutUnit = function(unit) {
     let result;
@@ -140,63 +104,63 @@ if(result.toString().includes('/')){
         result = "gallon(s)";
         break; 
       case "l":
+      case "L":
         result = "liter(s)";
-        break; 
+        break;
       default:
         break;
     }
 
     return result;
   };
-
-
+  
   this.convert = function(initNum, initUnit) {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
-    let result;
-
-    
+    var result;
+       
   if (initUnit === "mi")
-   {let getKm = eval(initNum*miToKm).toFixed(5)
-      result = getKm
-    return result
-    }
-
-if (initUnit === "km")
-    { let getMi = eval(initNum/miToKm).toFixed(5)
-      result = getMi
-      return result 
-    } 
-
-if (initUnit === "gal")
-    { let getLiter = eval(initNum*galToL).toFixed(5)
-      result = getLiter
-      return result 
-    }
-
-if (initUnit === "l")
-  { let getGallon = eval(initNum/galToL).toFixed(5)
-    result = getGallon
-    return result 
-  }
-if (initUnit === "lbs")
-  { let getPounds = eval(initNum*lbsToKg).toFixed(5)
-    result = getPounds
-    return result 
-  }
-if (initUnit === "kg")
-  { let getKgs = eval(initNum/lbsToKg).toFixed(5)
-    result = getKgs
-    return result 
-  }    
-    return result;
-  };
-
+    {let getKm = eval(initNum*miToKm).toFixed(5)
+       result = getKm
+     return result
+     }
  
+ if (initUnit === "km")
+     { let getMi = eval(initNum/miToKm).toFixed(5)
+       result = getMi
+       return result 
+     } 
+ 
+ if (initUnit === "gal")
+     { let getLiter = eval(initNum*galToL).toFixed(5)
+       result = getLiter
+       return result 
+     }
+ 
+ if (initUnit === "L" || initUnit === "l")
+   { let getGallon = eval(initNum/galToL).toFixed(5)
+     result = getGallon
+     return result 
+   }
+ if (initUnit === "lbs")
+   { let getPounds = eval(initNum*lbsToKg).toFixed(5)
+     result = getPounds
+     return result 
+   }
+ if (initUnit === "kg")
+   { let getKgs = eval(initNum/lbsToKg).toFixed(5)
+     result = getKgs
+     return result 
+   }  
+  };
+  
   this.getString = function(initNum, initUnit, returnNum, returnUnit) {
     let result;
-    
+ 
+
+    result = `${initNum} ${this.spellOutUnit(initUnit)} converts to ${returnNum} ${this.spellOutUnit(returnUnit)}`
+    //{ initNum: 3.1, initUnit: 'mi', returnNum: 4.98895, returnUnit: 'km', string: '3.1 miles converts to 4.98895 kilometers' }
     return result;
   };
   
--- routes/api.js
+++ routes/api.js
@@ -15,9 +15,15 @@ module.exports = function (app) {
     let returnUnit = convertHandler.getReturnUnit(initUnit);
     let toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
  
-    if (initUnit === "invalid unit" && initNum === 1)
-    {res.json("invalid unit")}
 
+
+    if(initNum === 'invalid number' && initUnit === 'invalid unit'){
+      res.json('invalid number and unit')
+    }
+
+    if (input === null || input === 0)
+    {res.json("invalid input")}
+  
     if(initNum === 'invalid number'){
       res.json('invalid number')
     }  
@@ -25,15 +31,16 @@ module.exports = function (app) {
     if(initUnit === 'invalid unit'){
       res.json('invalid unit')
     }  
-    console.log(initNum)
+  
     let resObj = {}
-    resObj["initNum"] = initNum
-    resObj["initUnit"] = initUnit
-    resObj["returnNum"] = returnNum
-    resObj["returnUnit"] = returnUnit
-    resObj["string"] = toString
-
-    res.json(resObj)
-
+    resObj['initNum'] = initNum
+    resObj['initUnit'] = initUnit
+    resObj['returnNum'] = returnNum
+    resObj['returnUnit'] = returnUnit
+    resObj['string'] = toString
+  
+    res.json(resObj);
+    //{ initNum: 3.1, initUnit: 'mi', returnNum: 4.98895, returnUnit: 'km', string: '3.1 miles converts to 4.98895 kilometers' }
+  
   })
 };
