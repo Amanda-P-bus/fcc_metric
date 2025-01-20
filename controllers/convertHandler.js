df8d5237431e649c8b365341e636e47e8e963921
Amanda Pollard <AmandaPhere@gmail.com>
2025-01-20 03:34:46 -0500

    Revert "feat: all output is correctly accepting, rejecting, and converting. trying to figure out why I'm getting errors for some rejections in the node terminal"
    This reverts commit a0d25914a38c12f6bcd378b8e7580f91294795c6.

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -2,10 +2,66 @@ function ConvertHandler() {
   
   this.getNum = function(input) {
     let result;
+<<<<<<< HEAD
     
     return result;
   };
   
+=======
+    result = numRes;
+
+//if there's no unit, reject
+if (input.match(/[0-9]$/)) 
+  { return "invalid unit" } 
+
+//if it doesn't start with a number
+  if (numRes.match(/^[0-9]/) === null)
+  {return "invalid number"}
+let numReg = /\d/
+  //if there's no number, start with 1 as a base unit
+      if (numReg.test(result) === false)
+        { result = 1; return result}
+
+//check for special characters
+let stringify = input.toString();
+    //if doesn't end with a number
+
+
+//deal with division   
+if(result.toString().includes('/')){
+  let values = result.toString().split('/')
+  let perCheck = result.toString().split('.')
+
+//if there's more than one "/"  
+  if(values.length != 2)
+    { return 'invalid number'}
+
+//if there's more than 2 "." for every "/"
+  if ((perCheck.length - values.length) > 1)
+    { return "invalid number"}
+
+//for rejecting values with too mamy "."
+    if (numRes.includes("."))
+    {
+      //if value[0] has more than one "."
+      if (values[0].split(".").length > 2)
+      {return "invalid number"}
+      //if value[1] has more than one "."
+      if (values[1].split(".").length > 2)
+        {return "invalid number"}
+
+    }
+
+  values[0] = parseFloat(values[0])
+  values[1] = parseFloat(values[1])
+  result = parseFloat((values[0]/values[1]).toFixed(5)) 
+}
+
+    return result
+};
+
+
+>>>>>>> parent of a0d2591 (feat: all output is correctly accepting, rejecting, and converting. trying to figure out why I'm getting errors for some rejections in the node terminal)
   this.getUnit = function(input) {
     let result;
     
--- routes/api.js
+++ routes/api.js
@@ -7,4 +7,34 @@ module.exports = function (app) {
   
   let convertHandler = new ConvertHandler();
 
+  app.route("/api/convert").get((req, res) => {
+    let input = req.query.input;
+    let initNum = convertHandler.getNum(input);
+    let initUnit = convertHandler.getUnit(input);
+    let returnNum = convertHandler.convert(initNum, initUnit);
+    let returnUnit = convertHandler.getReturnUnit(initUnit);
+    let toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+
+
+
+res.json(convertHandler.getNum(input))
+
+
+//  convertHandler.getNum(input);
+//  convertHandler.getUnit(input);
+
+ // res.json(convertHandler.getNum(input));
+
+    /*
+    convertHandler.getNum()
+    convertHandler.getUnit()
+    
+    convertHandler.getReturnUnit()
+    convertHandler.spellOutUnit()
+    convertHandler.convert()
+    convertHandler.getString()
+    */
+
+
+  })
 };
